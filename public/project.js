2696a9909fa4ce1f86eea4cc31769da617bcbccd
Athif Najeeb <athifnajeeb@gmail.com>
2025-04-14 20:05:47 +0530

    portfolio initial commit

---
+++ public/project.js
@@ -0,0 +1,9 @@
+//add projects here for portfolio
+
+let projects = [
+    {
+        name: "Social media app",
+        tags: "#fullstack, #javascript, #vue, #asp.net, #mvc",
+        image: "project.png",
+    }
+]
